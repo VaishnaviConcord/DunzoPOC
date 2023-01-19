a3008444a0f9499aa8420a99217a2eb66cf353f1
mohdshoaib59x <mohdshoaib1317@gmail.com>
2023-01-19 17:29:39 +0530

    NewDelivery and TakeCam

--- my-app/Screens/NewDelivery/index.js
+++ my-app/Screens/NewDelivery/index.js
@@ -1,25 +1,64 @@
-import { View, Text, Button } from "react-native";
+import { Text, View,TextInput, Image,Button } from "react-native";
+
+import styles from "./styles";
 import { useDispatch, useSelector } from "react-redux";
 import { setPickupAddress , setDeliveryAddress} from "../../store/reducers/currentOrder";
 
+
 const NewDelivery = () => {
   const { pickUpAddress, deliveryAddress } = useSelector(
-    (state) => state.currentOrder
-  );
-
-  const dispatch = useDispatch()
-  const updatePickupAddress = () => {
-    dispatch(setPickupAddress("planet earth"))
-  };
-  const updateDeliveryAddress = () => {
-    dispatch(setDeliveryAddress("planet mars"))
-  };
+        (state) => state.currentOrder
+      );
+    
+      const dispatch = useDispatch()
+      const updatePickupAddress = () => {
+        dispatch(setPickupAddress("planet earth"))
+      };
+      const updateDeliveryAddress = () => {
+        dispatch(setDeliveryAddress("planet mars"))
+      };
+  
   return (
-    <View style={{ marginTop: 100 }}>
-      <Text>{pickUpAddress}</Text>
-      <Text>{deliveryAddress}</Text>
-      <Button title="Update Pickup Address" onPress={updatePickupAddress} />
-      <Button title="Update Delivery Address" onPress={updateDeliveryAddress} />
+    <View>
+      <View style={[styles.includestyle]}>
+        {/* <Image
+            style={[styles.imagestyle]}
+            source={require("assets/eye1.png")}
+          /> */}
+
+        <Text style={{ color: "#A9A9A9" }}>Pickup address*</Text>
+        <TextInput style={{ fontWeight: "bold" }}>Search pickup location { pickUpAddress }</TextInput>
+        <Button title="Update Pickup Address" onPress={updatePickupAddress} />
+      </View>
+
+      <View style={[styles.includestyle]}>
+        {/* <Image
+            style={[styles.imagestyle]}
+            source={require("assets/eye1.png")}
+          /> */}
+
+        <Text style={{ color: "#A9A9A9" }}>Delivery address*</Text>
+        <TextInput style={{ fontWeight: "bold" }}>Search delivery location { deliveryAddress }</TextInput>
+        <Button title="Update Delivery Address" onPress={updateDeliveryAddress}/>
+      </View>
+
+      {/* <Button title="Update Pickup Address" onPress={updatePickupAddress} />
+       <Button title="Update Delivery Address" onPress={updateDeliveryAddress}/> */}
+
+      <View style={[styles.includestyle]}>
+        {/* <Image
+            style={[styles.imagestyle]}
+            source={require("assets/eye1.png")}
+          /> */}
+
+        <Text style={{ color: "#A9A9A9" }}>Select Package contents*</Text>
+        <TextInput style={{ fontWeight: "bold" }}>e.g.Food,Document</TextInput>
+      </View>
+
+      <View style={styles.buttonText}>
+        {/* <Text>Cancel</Text> */}
+        <Text>Done</Text>
+      </View>
     </View>
   );
 };
