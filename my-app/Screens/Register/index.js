d92a3312605f3c5712fb580c3b586ee60f057d6b
vishnupriya <c009304@footlocker.com>
2023-01-20 16:13:48 +0530

    formatted the code

--- my-app/Screens/Register/index.js
+++ my-app/Screens/Register/index.js
@@ -2,47 +2,42 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TextInput, Pressable, Image, ActivityIndicator } from 'react-native';
 import React, { useState } from "react";
 
-
 export default function Register() {
+    var userName, userEmail, userPassword, userPhoneNumber;
 
-    var userName,userEmail,userPassword,userPhoneNumber;
-    
-
-    const InputView = ({onChangeText, placeholder, secureTextEntry, imagename,name,text})  => {
-
+    const InputView = ({ onChangeText, placeholder, secureTextEntry, imagename, name, text }) => {
         return (
-        <View style={styles.SectionStyle}>
-            <Image
-    source={imagename}
-    style={styles.ImageStyle}
-     />
-           <TextInput style={{ flex: 1 }}
-            secureTextEntry={secureTextEntry}
-            placeholder={placeholder}
-            autoCorrect={false}
-            name={name}
-            onChangeText={text => handleChange({ name, text })}
-            />
+            <View style={styles.SectionStyle}>
+                <Image
+                    source={imagename}
+                    style={styles.ImageStyle}
+                />
+                <TextInput style={{ flex: 1 }}
+                    secureTextEntry={secureTextEntry}
+                    placeholder={placeholder}
+                    autoCorrect={false}
+                    name={name}
+                    onChangeText={text => handleChange({ name, text })}
+                />
             </View>
         );
+    }
 
-    }
- 
     handleChange = (event) => {
-    if(event.name == "username") {
-        userName = event.text
+        if (event.name == "username") {
+            userName = event.text
+        }
+        else if (event.name == "password") {
+            userPassword = event.text
+        }
+        else if (event.name == "email") {
+            userEmail = event.text
+        }
+        else if (event.name == "phonenumber") {
+            userPhoneNumber = event.text
+        }
     }
-    else if(event.name == "password") {
-        userPassword = event.text
-    }
-    else if(event.name == "email") {
-        userEmail = event.text
-    }
-    else if(event.name == "phonenumber") {
-        userPhoneNumber = event.text
-    }
-    }
-    
+
     const signUpAction = () => {
         if (!userName) {
             alert('Please Enter Name');
@@ -72,7 +67,7 @@ export default function Register() {
             alert('Please Enter Valid Phone number');
             return;
         }
-        
+
         const params = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -87,136 +82,98 @@ export default function Register() {
             .then(response => response.json())
             .then(data => {
                 alert("User Registered")
-                this.name.clear();
-                this.email.clear();
-                this.phonenumber.clear();
-                this.password.clear();
-                setUserName("")
-                setUserEmail("")
-                setUserPassword("")
-                setUserPhoneNumber("")
             }
             );
-        };
+    };
 
-        
+    return (
+        <View style={styles.container}>
+            <>
+                <Text style={styles.signuptext}>Sign Up</Text>
 
-        return (
-            <View style={styles.container}>
-                <>
-                    <Text style={styles.signuptext}>Sign Up</Text>
-                    
-<InputView placeholder="Enter Your Name Here"
-   secureTextEntry="false"
-   onChange={this.handleChange}
-   name="username"
-  imagename={require('../Assets/name.png')}> </InputView>
+                <InputView placeholder="Enter Your Name Here"
+                    secureTextEntry="false"
+                    onChange={this.handleChange}
+                    name="username"
+                    imagename={require('../Assets/name.png')}> </InputView>
 
-<InputView placeholder="Enter Your Email Here"
-   secureTextEntry="false"
-   onChange={this.handleChange}
-   name="email"
-  imagename={require('../Assets/email.png')}> </InputView>
+                <InputView placeholder="Enter Your Email Here"
+                    secureTextEntry="false"
+                    onChange={this.handleChange}
+                    name="email"
+                    imagename={require('../Assets/email.png')}> </InputView>
 
-<InputView placeholder="Enter Your Password Here"
-   secureTextEntry="true"
-   onChange={this.handleChange}
-   name="password"
-  imagename={require('../Assets/password.png')}> </InputView>
+                <InputView placeholder="Enter Your Password Here"
+                    secureTextEntry="true"
+                    onChange={this.handleChange}
+                    name="password"
+                    imagename={require('../Assets/password.png')}> </InputView>
 
-<InputView placeholder="Enter Your Phone Number Here"
-   secureTextEntry="false"
-   onChange={this.handleChange}
-   name="phonenumber"
-  imagename={require('../Assets/phonenumber.png')}> </InputView>
+                <InputView placeholder="Enter Your Phone Number Here"
+                    secureTextEntry="false"
+                    onChange={this.handleChange}
+                    name="phonenumber"
+                    imagename={require('../Assets/phonenumber.png')}> </InputView>
 
-<Pressable style={styles.signup} onPress={signUpAction}>
-      <Text style={styles.title}>{"Sign Up"}</Text>
-    </Pressable>
-                </>
-                <StatusBar style="auto" />
-            </View>
-        );
-    }
+                <Pressable style={styles.signup} onPress={signUpAction}>
+                    <Text style={styles.title}>{"Sign Up"}</Text>
+                </Pressable>
+            </>
+            <StatusBar style="auto" />
+        </View>
+    );
+}
 
-    const styles = StyleSheet.create({
-        container: {
-            backgroundColor: '#fff',
-            alignItems: 'stretch',
-            padding: 20,
-            flex: 1,
-        },
-        input: {
-            backgroundColor: '#fff',
-            borderColor: '#EDECEC',
-            borderWidth: 2,
-            padding: 10,
-            borderBottomWidth: 0,
-            fontWeight: "bold",
-        },
-        passwordInput: {
-            backgroundColor: '#fff',
-            borderColor: '#EDECEC',
-            borderWidth: 2,
-            padding: 10,
-        },
-        text: {
-            justifyContent: 'center',
-            textAlign: 'center',
-            marginBottom: 5,
-            fontSize: 20,
-            fontWeight: 'bold',
-        },
-        referralcode: {
-            fontSize: 15,
-            marginTop: 20,
-            fontWeight: 'bold',
-            color: "#2265E5",
-        },
-        signup: {
-            fontSize: 20,
-            fontWeight: 'bold',
-            borderRadius: 10,
-            height: 50,
-            alignItems: "center",
-            justifyContent: "center",
-            backgroundColor: "#FC8019",
-            marginTop: 100,
-            marginLeft:10,
-            marginRight:10,
-        },
-        title: {
-            fontSize: 20,
-            fontWeight: 'bold',
-            color: "#FFFFFF",
-        },
-        signuptext: {
-            textAlign: 'center',
-            fontSize: 24,
-            marginBottom: 30,
-            fontWeight: "bold",
-            marginTop: 50,
-            color: '#FC8019',
-        },    
-        SectionStyle: {
-            flexDirection: 'row',
-            justifyContent: 'center',
-            alignItems: 'center',
-            backgroundColor: '#f0f4fc',
-            borderWidth: 0.5,
-            borderColor: '#f0f4fc',
-            height: 50,
-            borderRadius: 5,
-            margin: 10,
-        },
-        ImageStyle: {
-            padding: 10,
-            margin: 15,
-            height: 20,
-            width: 20,
-            resizeMode: 'stretch',
-            alignItems: 'center',
-        },
-    });
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#fff',
+        alignItems: 'stretch',
+        padding: 20,
+        flex: 1,
+    },
+   signup: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        borderRadius: 10,
+        height: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#FC8019",
+        marginTop: 100,
+        marginLeft: 10,
+        marginRight: 10,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: "#FFFFFF",
+    },
+    signuptext: {
+        textAlign: 'center',
+        fontSize: 24,
+        marginBottom: 30,
+        fontWeight: "bold",
+        marginTop: 50,
+        color: '#FC8019',
+    },
+    SectionStyle: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#f0f4fc',
+        borderWidth: 0.5,
+        borderColor: '#f0f4fc',
+        height: 50,
+        borderRadius: 5,
+        margin: 10,
+    },
+    ImageStyle: {
+        padding: 10,
+        margin: 15,
+        height: 20,
+        width: 20,
+        resizeMode: 'stretch',
+        alignItems: 'center',
+    },
+});
 
- 
