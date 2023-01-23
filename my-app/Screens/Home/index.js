ff2a9d0b6b7ac0e52b9e1c3c9864ab53efbddf85
Sarita S <Sarita S>
2023-01-23 14:23:11 +0530

    Added home page

--- my-app/Screens/Home/index.js
+++ my-app/Screens/Home/index.js
@@ -1,43 +1,56 @@
-import { useNavigation } from '@react-navigation/core'
-import React from 'react'
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-// import { auth } from '../firebase'
-import styles from "./styles";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image
+} from 'react-native';
+import Icon from "react-native-vector-icons/FontAwesome";
 
 const Home = () => {
-  const navigation = useNavigation()
-
-  // const handleSignOut = () => {
-  //   auth
-  //     .signOut()
-  //     .then(() => {
-  //       navigation.replace("Login")
-  //     })
-  //     .catch(error => alert(error.message))
-  // }
-
-  const handleSignOut = () => {
-    // if(email === "mohd" && password === "123"){
-    //   Alert.alert(`thank you ${email}`)
-      navigation.replace("Login")
-    // }else{
-    //   Alert.alert(`email and password is incorrect`)
-    // }
-  }
-
   return (
-    <View style={styles.container}>
-      {/* <Text>Email: {auth.currentUser?.email}</Text> */}
-      <Text>Email</Text>
-      <TouchableOpacity
-        onPress={handleSignOut}
-        style={styles.button}
-      >
-        <Text style={styles.buttonText}>Sign out</Text>
-      </TouchableOpacity>
-    </View>
-  )
+      <>
+          <View style={
+              styles.container
+          }>
+              <Image
+            style={styles.imageStyles}
+            source={require("../../assets/home.jpg")}
+          />
+              <View style={styles.iconRow}>
+                  <Icon name="user"
+                      size={35}
+                      />                   
+              </View>
+              <View style={styles.iconRowText}><Text style={styles.iconText}>USER</Text></View>
+
+          </View>
+      </>
+  );
 }
-
+const styles = StyleSheet.create({
+  container: {
+      width: 500,
+      height: 700,
+      backgroundColor: "#FFFFFF"
+  },
+  imageStyles: {
+      width: 350,
+      height: 270,
+      marginTop: 50
+  },
+  iconRow:{
+      flexDirection:"row",
+      marginTop:200,
+      marginLeft:50,
+      marginRight:50
+  },
+  iconRowText:{
+    flexDirection:"row",
+  },
+  iconText:{
+ marginLeft:49,
+ fontSize:13,
+ fontWeight:"bold"
+  }
+})
 export default Home
-
